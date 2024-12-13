c50ae741292df43735ce50c4e58c85ff71530068
roanmorgan53 <roanlm007@gmail.com>
2024-12-12 19:23:23 -0800

    Added comments digs in all 4 directions

--- stripmine.lua
+++ stripmine.lua
@@ -1,26 +1,42 @@
+function digAllDirections()
+    turtle.digUp()
+    turtle.digDown()
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.turnRight()
+    turtle.turnRight()
+    turtle.dig()
+    turtle.turnLeft()
+end
+
 function dig_column()
+    -- In order to counter gravel or sand inhibiting movement and the alignment of the turtle, the turtle will dig forward until it can move forward
     local pass = false
     while not pass do
         turtle.dig()
         pass = turtle.forward()
     end
-    turtle.digUp()
-    turtle.digDown()
+    
+    digAllDirections()
+    
 end
 
 function strip(len)
     turtle.refuel()
     print("Refueling...")
 
+    -- Dig out the column the specified depth
     for i = 1, len do
         dig_column()
         print("Stripping... Progress: " .. i .. "/" .. len)
     end
 
+    -- Turn around and select torces in the second slot
     turtle.turnRight()
     turtle.turnRight()
     turtle.select(2)
 
+    -- Place torches every 10 blocks
     for i = 1, len do
         turtle.forward()
         
@@ -30,7 +46,10 @@ function strip(len)
     end
 end
 
+-- Main
 print("Beginning stripmine.lua")
+print("*** REMINDER -- Have you put coal in the first slot and torches in the second slot? ***")
+
 print("Input your desired length for the stripmine:")
 local length = tonumber(io.read())
 
